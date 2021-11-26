ee36de7bc313f909254ae9461f8605ddec8d2b66
Kauana Cavalheiro Carneiro <72306197+KauanaCnr@users.noreply.github.com>
2021-11-26 15:43:55 -0300

    Update login.spec

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,5 +1,27 @@
+
+/// <reference types="cypress" /> 
 import LoginMercado from '../pages/login.pages.js'
+describe("Teste de Login Ecommerce Mercado Online", () =>{
+    beforeEach(() =>{
+        LoginMercado.abrirlogin()
+    })
 
+    it('Verificar existencia e visibilidade de elementos pagina login  Ecommerce Mercado Online', () =>{
+      LoginMercado.validarElementos()
+    })
+  
+     it('Verificar existencia e visibilidade de elementos pagina login e fazer o mesmo no  Ecommerce Mercado Online', () =>{
+      LoginMercado.validarLogin()
+    })
 
+      })
+      it('verificar a existencia da mensagem de erro apos um login sem email', () =>{
+        LoginMercado.validarLoginIncorreto('Dados inválidos, digite novamente!')
+      })
+      it('verificar a existencia da mensagem de erro apos um login sem senha', () =>{
+        LoginMercado.validarLoginIncorreto('Autenticação incorreta.')
+      })
 
-describe("Tes")
+     
+  
+})
