b10ec820ff315311a8776dca3af42a53de23bbde
Kauana Cavalheiro Carneiro <72306197+KauanaCnr@users.noreply.github.com>
2021-11-26 15:52:05 -0300

    Update cadastro.spec

---
+++ cypress/integration/cadastro.spec.js
@@ -0,0 +1,38 @@
+/// <reference types="cypress" /> 
+import CadastromMercado from "../pages/cadastro.pages";
+
+describe("Teste de Cadastro  Ecommerce Mercado Online", () =>{
+    beforeEach(() =>{
+    CadastromMercado.abrirCadastro()
+        
+    })
+    it('Varificar existecia de elementos na pagina de cadastro pessoa fisica', () =>{
+       CadastromMercado.validarElementosPF()
+    })
+     it('Varificar existecia de elementos na pagina de cadastro pessoa juridica', () =>{
+       CadastromMercado.validarElementosPJ()
+    }) 
+    it('Cadastrar novo usuario', () =>{
+        CadastromMercado.cadastrarCliente()
+    })
+    it('Cadastrar um usuario já cadastrado', () =>{
+        CadastromMercado.cadastrarClienteInvalido()
+    })
+    it('Cadastrar um usuario sem cpf', () =>{
+       CadastromMercado.cadastrarClienteSemCPF()
+    })
+    it('Cadastrar um usuario sem nome', () =>{
+      CadastromMercado.cadastrarUsuarioSemNome()
+    })
+    it('Cadastrar um usuario sem email', () =>{
+       CadastromMercado.cadastrarUsuarioSemEmail()
+    })
+    it('Cadastrar um usuario sem senha', () =>{
+       CadastromMercado.cadastrarUsuarioSemSenha()
+    })
+    it('Cadastrar um usuario sem prencher os campos', () =>{
+       CadastromMercado.cadastrarUsuarioComCamposVazios()
+    })
+    
+     
+})
