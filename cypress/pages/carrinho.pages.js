39d23408f7d268341d344c9e6136657efe36bc18
Kauana Cavalheiro Carneiro <kauanacavalheiro2001@gmail.com>
2021-11-26 23:25:13 -0300

    Carrinho

--- cypress/pages/carrinho.pages.js
+++ cypress/pages/carrinho.pages.js
@@ -5,11 +5,11 @@ import FRUTAS from '../components/ofertas.elements'
 
 export default class AdicionarCarrinho extends Base {
 
-  	static abrirFrutas(){
-  		cy.visitar()
-  		super.clickOnElement(BTN_FRUTAS)
+  static abrirFrutas(){
+  	cy.visitar()
+  	super.clickOnElement(BTN_FRUTAS)
         super.validarUrl('/frutas')
   	}
-    static validarElementos(){
+    static validarElementosFrutas(){
      
     }
--- cypress/pages/components/ofertas.elements.js
+++ cypress/pages/components/ofertas.elements.js
@@ -1,8 +1,15 @@
-export const CATEGORIAS ={
+export const FRUTAS ={
 	BTN_FRUTAS     : '//*[@id="sidebar"]/div[2]/ul/li[1]/a/span',
-	BTN_CITRICOS   : '//*[@id="sidebar"]/div[2]/ul/li[1]/div/div/div[1]/ul/li[1]/a',
-	BTN_VERMELHAS  : '//*[@id="sidebar"]/div[2]/ul/li[1]/div/div/div[1]/ul/li[2]/a',
-	BTN_VERFRUTAS  : '//*[@id="sidebar"]/div[2]/ul/li[1]/div/div/div[3]/a',
-	BTN_ADDFRUTAS  : '//*[@id="sidebar"]/div[2]/ul/li[1]/div/div/div[2]/div/div/div/div[3]/div[2]/button'
+	BTN_CITRICAS   : '//*[@id="main"]/div[5]/div[1]/a',
+	BTN_RARAS      : '//*[@id="main"]/div[5]/div[6]/a',
+	BTN_SALADA     : '//*[@id="main"]/div[5]/div[3]/a',
+	BTN_ORGANICAS  : '//*[@id="main"]/div[5]/div[8]/a',  
+	BTN_VERMELHAS  : '//*[@id="main"]/div[5]/div[2]/a',
+	BTN_AMARELAS   : '//*[@id="main"]/div[5]/div[4]/a',
+	BTN_SECAS      : '//*[@id="main"]/div[5]/div[5]/a',
+	BTN_ESTACAO    : '//*[@id="main"]/div[5]/div[7]/a',
+	BTN_ADICIONAR  : '//*[@id="main"]/div[6]/div[2]/div[3]/div/div[3]/div[2]/button',
+	BTN_MAIS       : '//*[@id="main"]/div[6]/div[2]/div[1]/div/div[3]/div[2]/div/button[2]',
+	BTN_MENOS      : '//*[@id="main"]/div[6]/div[2]/div[1]/div/div[3]/div[2]/div/button[1]'
 
 }
